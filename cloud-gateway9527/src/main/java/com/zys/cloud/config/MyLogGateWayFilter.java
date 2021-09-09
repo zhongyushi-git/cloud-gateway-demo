aeffed91247a095e04b458e972e6ef7b1e03a4d1
zhongyushi <1916008067@qq.com>
2021-09-09 14:13:58 +0800

    拦截器优化

--- cloud-gateway9527/src/main/java/com/zys/cloud/config/MyLogGateWayFilter.java
+++ cloud-gateway9527/src/main/java/com/zys/cloud/config/MyLogGateWayFilter.java
@@ -1,15 +1,22 @@
 package com.zys.cloud.config;
 
+import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpHeaders;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -22,11 +29,16 @@ public class MyLogGateWayFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String token = exchange.getRequest().getHeaders().getFirst("token");
-        log.error(token);
         //非法用户直接拦截
-        if(token == null){
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
+        if (token == null) {
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            Map<String, Object> map = new HashMap<>();
+            map.put("code", 401);
+            map.put("msg", "未授权，无法访问");
+            byte[] errs = JSON.toJSONBytes(map);
+            DataBuffer buffer = response.bufferFactory().wrap(errs);
+            return response.writeWith(Flux.just(buffer));
         }
         return chain.filter(exchange);
     }
